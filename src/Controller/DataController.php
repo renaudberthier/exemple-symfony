f124d6a13cb884dad2b003d2f7ffae042ccb8483
Renaud Berthier <renaudberthier@gmail.com>
2022-04-12 16:24:25 +0200

    Correction de l'exercice

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DataController extends AbstractController
 {
-    #[Route('/test-formulaire', name: 'test_formulaire')]
+    #[Route('/test-formulaire', name: 'test_form')]
     public function index(Request $request): Response
     {
         // Données en POST : $request->request
@@ -18,6 +18,51 @@ class DataController extends AbstractController
         // $firstname = $request->query->get('firstname');
         return $this->render('data/index.html.twig', [
             "firstname" => $firstname
+        ]);
+    }
+
+    #[Route('/correction-formulaire', name: 'exercise_form')]
+    public function exercise(Request $request) : Response
+    {
+        // Récupérer toutes les valeurs :
+        // $all = $request->request->all();
+        // var_dump($all);
+
+        // Récupérer toutes les clefs :
+        // $keys = $request->request->keys();
+        // var_dump($keys);
+
+        // Une valeur par défaut :
+        // $age = $request->request->getInt('age', 45);
+
+        $firstname = $request->request->get('firstname');
+        $lastname = $request->request->get('lastname');
+        $age = $request->request->getInt('age');
+
+        $test = "Toto";
+        $estcequelavaleurestvide = !$test; // false -> 0
+        $estcequelavaleurestpleine = !!$test; // true -> 1
+        $a = !!!$test; // = !$test
+        
+        // Etape 1 : Transformer $test en booléen
+        $test = (bool) $test; // true
+
+        $error = null;
+        if(
+            // Si y'a firstname
+            // Ca vaut true (1)
+            // Sinon false  (0)
+            !!$firstname + !!$lastname + !!$age > 0 && 
+            !!$firstname + !!$lastname + !!$age < 3
+        ) {
+            $error = "Il manque des champs";
+        }
+
+        return $this->render('data/exercise.html.twig', [
+            "firstname" => $firstname,
+            "lastname" => $lastname,
+            "age" => $age,
+            "error" => $error
         ]);
     }
 }
